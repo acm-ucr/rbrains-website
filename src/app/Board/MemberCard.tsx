cd78d3e433d5014e2bc6363877caa0815d0ee486
Nathan Matsuo <nathan@ucr-secure-21-10-13-133-146.wnet.ucr.edu>
2025-04-15 21:02:54 -0700

    added MemberCard, needs to have correct font and zoomed picture

--- src/app/Board/MemberCard.tsx
+++ src/app/Board/MemberCard.tsx
@@ -2,11 +2,19 @@ import React from "react";
 
 const MemberCard = () => {
   return (
-    <div className="rounded-[25px] border-[2px] border-[#000000] p-5 w-[200px] h-[250px]">
-        President
-        <img src="/images/Rishitha Kona.jpg" alt="Rishitha Kona"></img>
-        <p>Rishitha Kona</p>
-        <p>(she/her/hers) 3rd year Psychology and Business double major</p>
+    <div className="h-[367px] w-[298px] rounded-[25px] border-[2px] border-[#000000] p-5 text-center font-serif">
+      <p>
+        <b>President</b>
+      </p>
+      <img
+        /*className="h-[159px] w-[159px]" */ src="/images/Rishitha Kona.jpg"
+        alt="Rishitha Kona"
+      ></img>
+      <p>
+        <b>Rishitha Kona</b>
+      </p>
+      <p className="font-Times">(she/her/hers)</p>
+      <p>3rd year Psychology and Business double major</p>
     </div>
   );
 };
