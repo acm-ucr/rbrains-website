cdd0f9358672d0932b4d53729c72cd8dffb1caf6
Nathan Matsuo <nathan@ucr-secure-21-10-13-133-146.wnet.ucr.edu>
2025-04-15 18:46:16 -0700

    working on member card, need to go to class

---
+++ src/app/Board/MemberCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MemberCard = () => {
+  return (
+    <div className="rounded-[25px] border-[2px] border-[#000000] p-5 w-[200px] h-[250px]">
+        President
+        <img src="/images/Rishitha Kona.jpg" alt="Rishitha Kona"></img>
+        <p>Rishitha Kona</p>
+        <p>(she/her/hers) 3rd year Psychology and Business double major</p>
+    </div>
+  );
+};
+
+export default MemberCard;
---
+++ src/app/Board/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MemberCard from "../../app/Board/MemberCard";
+
+const BoardPage = () => {
+  return (
+    <div>
+      <MemberCard />
+    </div>
+  );
+};
+
+export default BoardPage;
