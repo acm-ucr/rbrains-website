cbdaf8fe78b9e8e94ae76653bcc9d8cdb30715f9
4skate <tyler06.t.e@gmail.com>
2025-04-16 16:11:48 -0700

    test

---
+++ src/Components/Home/HomeLanding.tsx
@@ -0,0 +1,12 @@
+const HomeLanding = () => {
+  return (
+    <div>
+      <img
+        src="/Public/Images/title_page.png"
+        alt="Title_Page"
+        width="1498.53"
+        height="430"
+      ></img>
+    </div>
+  );
+};
