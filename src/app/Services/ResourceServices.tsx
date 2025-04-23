1ffe8b27cd1c48d4d62cfb299e16f07ac5bf4186
Kelly Ramirez <157239272+kellyramirezz@users.noreply.github.com.>
2025-04-22 22:48:25 -0700

    Added a Service file and inside a page.tsx and ResourceService.tsx

---
+++ src/app/Services/ResourceServices.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ResourceServices = () => {
+  return (
+    <div className="relative h-screen w-full">
+      <div className="h-971px w-1116px absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-[40px] border-[4px] border-[#93B48F] bg-[#FFFBF9] text-center font-[Besley] text-[24px]">
+        <div className="mt-[46px] mr-[29px] ml-[21px]">
+          <div className="font-[800] text-[#D66E6D]">
+            UCR's Health Promotion and Education Department
+          </div>
+          <div className="font-[400] text-[#442F2C]">
+            <div className="">
+              The Well provides students with health education tools, knowledge,
+              resources, and supplies to keep themselves as healthy as possible
+              so they can succeed at UCR and beyond
+            </div>
+            <div>Learn more about resources</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResourceServices;
---
+++ src/app/Services/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ResourceServices from "@/app/Services/ResourceServices";
+
+const ServPage = () => {
+  return (
+    <div>
+      <ResourceServices />
+    </div>
+  );
+};
+
+export default ServPage;
