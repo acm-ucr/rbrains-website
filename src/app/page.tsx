6bead2e77567935e07e5d28a76ad80a35ce387fa
4skate <tyler06.t.e@gmail.com>
2025-04-23 14:38:47 -0700

    confused about the exact position of the logo, due to the nav bar, also need help with sizing and positioning relative to the screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import HomeLanding from "@/Components/Home/HomeLanding";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       Hello World
+      <HomeLanding />
     </div>
   );
 };
