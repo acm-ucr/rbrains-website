51a0d72011a562de7b05870828e460b7a7556c6e
Nathan Matsuo <nathan@ucr-secure-21-10-13-135-120.wnet.ucr.edu>
2025-04-23 16:12:09 -0700

    updated membercard

--- src/app/components/MemberCard.tsx
+++ src/app/components/MemberCard.tsx
@@ -4,15 +4,15 @@ import Pic from "@/Public/Rishitha Kona.png";
 
 const MemberCard = () => {
   return (
-    <div className="h-1/5 w-1/6 rounded-3xl border-[2px] border-[#000000] p-5 text-center font-serif">
+    <div className="h-1/5 w-1/5 rounded-3xl border-[2px] border-[#000000] p-5 text-center font-serif bg-amber-300">
       <div className="text-4xl">President</div>
-      <div className="-mt-12">
+      <div className="">
         <Image
-          className="w-full scale-55"
+          className="size-3/4 mx-auto"
           src={Pic}
           alt="Rishitha Kona"
         ></Image>
-        <div className="-mt-12">
+        <div className="">
           <div className="text-2xl"> Rishitha Kona </div>
           <div>(she/her/hers) </div>
           <div>3rd year Psychology and Business double major </div>
