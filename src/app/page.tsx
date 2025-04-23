57ad27b248037c5ea52ccfab0b465237132d923d
4skate <tyler06.t.e@gmail.com>
2025-04-23 15:59:19 -0700

    Revert "4skate/home landing2"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import HomeLanding from "@/Components/Home/HomeLanding";
-
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       Hello World
-      <HomeLanding />
     </div>
   );
 };
