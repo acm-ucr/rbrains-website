e94d25774be2b126042c57446eb5e13b492e7728
Nathan Matsuo <nathan@ucr-secure-21-10-13-135-120.wnet.ucr.edu>
2025-04-23 19:13:31 -0700

    added grid and refactored info code, but used page.tsx

--- src/app/Board/page.tsx
+++ src/app/Board/page.tsx
@@ -3,8 +3,37 @@ import MemberCard from "@/app/components/MemberCard";
 
 const BoardPage = () => {
   return (
-    <div>
-      <MemberCard />
+    <div className="grid grid-cols-3 place-items-center gap-5 bg-amber-500">
+      <MemberCard
+        position="President"
+        name="Rishitha Kona"
+        pronouns="she/her/hers"
+        info="3rd year Psychology and Business double major"
+      />
+      <MemberCard
+        position="Secretary"
+        name="Abby Lazar"
+        pronouns="she/her/hers"
+        info="3rd year Psychology major, TFDP Film minor"
+      />
+      <MemberCard
+        position="Treasurer"
+        name="Sagey Moorjani"
+        pronouns="she/her/hers"
+        info="2nd year Psychology major"
+      />
+      <MemberCard
+        position="Public Relations Head"
+        name="Shriya patel"
+        pronouns="she/her/hers"
+        info="3rd-year Psychology major"
+      />
+      <MemberCard
+        position="Social Media Director"
+        name="Catherine Sanchez"
+        pronouns="she/her/hers"
+        info="4th year Psychology major, Public Policy minor"
+      />
     </div>
   );
 };
--- src/app/components/MemberCard.tsx
+++ src/app/components/MemberCard.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import Pic from "@/Public/Rishitha Kona.png";
+interface MemberCardProps {
+  position: string;
+  name: string;
+  pronouns: string;
+  info: string;
+}
 
-const MemberCard = () => {
+const MemberCard = ({ position, name, pronouns, info }: MemberCardProps) => {
   return (
-    <div className="border-[#000000 h-1/5 w-1/5 rounded-3xl border-[2px] p-5 text-center font-serif">
-      <div className="text-4xl">President</div>
+    <div className="h-full w-1/2 rounded-3xl border-[2px] border-black bg-amber-300 p-5 text-center font-serif">
+      <div className="text-4xl">{position}</div>
       <div className="">
         <Image
           className="mx-auto size-3/4"
           src={Pic}
           alt="Rishitha Kona"
         ></Image>
-        <div className="">
-          <div className="text-2xl"> Rishitha Kona </div>
-          <div>(she/her/hers) </div>
-          <div>3rd year Psychology and Business double major </div>
-        </div>
+        <div className="text-2xl"> {name} </div>
+        <div> ({pronouns}) </div>
+        <div> {info} </div>
       </div>
     </div>
   );
