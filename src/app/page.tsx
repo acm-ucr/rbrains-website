f431876b4b03cbb9ba477de43a3b821f0969580e
Eros846 <karkisarik.2021@gmail.com>
2025-04-24 19:02:24 -0700

    Made changes to correct file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
-// const Home = () => {
-//   return (
-//     <div className="flex h-screen w-screen items-center justify-center">
-//       Hello World
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-import Image from "next/image";
-
-const Component = () => {
+const Home = () => {
   return (
-    <div className="flex items-center justify-center bg-[#F9F5EF] p-6">
-      <Image
-        src="/StarLogo.webp"
-        alt="Star Logo"
-        className="object-contain"
-        width={300}
-        height={300}
-      />
+    <div className="flex h-screen w-screen items-center justify-center">
+      Hello World
     </div>
   );
 };
-export default Component;
+
+export default Home;
--- src/components/StarLogo.tsx
+++ src/components/StarLogo.tsx
@@ -4,7 +4,7 @@ const Component = () => {
   return (
     <div className="flex items-center justify-center bg-[#F9F5EF] p-6">
       <Image
-        src="/StarLogo.png"
+        src="/StarLogo.webp"
         alt="Star Logo"
         className="object-contain"
         width={300}
