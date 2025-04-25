6ea9ec613ab24a2614672f7ecd6974e64c9a5847
Eros846 <karkisarik.2021@gmail.com>
2025-04-24 18:58:10 -0700

    Changed StartLogo file format.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-const Home = () => {
+// const Home = () => {
+//   return (
+//     <div className="flex h-screen w-screen items-center justify-center">
+//       Hello World
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import Image from "next/image";
+
+const Component = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+    <div className="flex items-center justify-center bg-[#F9F5EF] p-6">
+      <Image
+        src="/StarLogo.webp"
+        alt="Star Logo"
+        className="object-contain"
+        width={300}
+        height={300}
+      />
     </div>
   );
 };
-
-export default Home;
+export default Component;
