f8195110eff98df747d186927885d90b1b9145d8
Kelly Ramirez <157239272+kellyramirezz@users.noreply.github.com.>
2025-04-28 01:33:36 -0700

    Updated ResourceServices.tsx and implemented feedback

--- src/app/Services/ResourceServices.tsx
+++ src/app/Services/ResourceServices.tsx
@@ -2,20 +2,18 @@ import React from "react";
 
 const ResourceServices = () => {
   return (
-    <div className="relative h-screen w-full">
-      <div className="h-971px w-1116px absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-[40px] border-[4px] border-[#93B48F] bg-[#FFFBF9] text-center font-[Besley] text-[24px]">
-        <div className="mt-[46px] mr-[29px] ml-[21px]">
-          <div className="font-[800] text-[#D66E6D]">
-            UCR's Health Promotion and Education Department
-          </div>
-          <div className="font-[400] text-[#442F2C]">
-            <div className="">
-              The Well provides students with health education tools, knowledge,
-              resources, and supplies to keep themselves as healthy as possible
-              so they can succeed at UCR and beyond
-            </div>
-            <div>Learn more about resources</div>
-          </div>
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex h-9/10 w-5/6 flex-col space-y-3 rounded-4xl border-4 border-[#93B48F] bg-[#FFFBF9] p-10 text-center font-[Besley]">
+        <div className="text-2xl font-semibold text-[#D66E6D]">
+          UCR's Health Promotion and Education Department
+        </div>
+        <div className="text-xl font-medium text-[#442F2C]">
+          The Well provides students with health education tools, knowledge,
+          resources, and supplies to keep themselves as healthy as possible so
+          they can succeed at UCR and beyond.
+        </div>
+        <div className="text-xl font-medium text-[#442F2C]">
+          Learn more about resources
         </div>
       </div>
     </div>
