a3b5ee00935d187f56a68a2b04f0e8a896de1af5
ACM UCR <fake@domain.com>
2025-04-29 05:35:03 +0000

    convert images to webp

--- src/data.ts
+++ src/data.ts
@@ -1,42 +1,42 @@
 export const data = [
   {
     id: 1,
-    image: "/images/IMG_0788.jpg",
+    image: "/images/IMG_0788.webp",
   },
   {
     id: 2,
-    image: "/images/IMG_0754.jpg",
+    image: "/images/IMG_0754.webp",
   },
   {
     id: 3,
-    image: "/images/IMG_0739.jpg",
+    image: "/images/IMG_0739.webp",
   },
   {
     id: 4,
-    image: "/images/IMG_0730.jpg",
+    image: "/images/IMG_0730.webp",
   },
   {
     id: 5,
-    image: "/images/IMG_0778.jpg",
+    image: "/images/IMG_0778.webp",
   },
   {
     id: 6,
-    image: "/images/IMG_0727.jpg",
+    image: "/images/IMG_0727.webp",
   },
   {
     id: 7,
-    image: "/images/IMG_0769.jpg",
+    image: "/images/IMG_0769.webp",
   },
   {
     id: 8,
-    image: "/images/IMG_0773.jpg",
+    image: "/images/IMG_0773.webp",
   },
   {
     id: 9,
-    image: "/images/IMG_0782.jpg",
+    image: "/images/IMG_0782.webp",
   },
   {
     id: 10,
-    image: "/images/IMG_0736.jpg",
+    image: "/images/IMG_0736.webp",
   },
 ];
