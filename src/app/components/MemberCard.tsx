d3c7044092b718495920cd4beb9e8ed666d187c3
Nathan Matsuo <nathan@ucr-secure-21-10-13-135-164.wnet.ucr.edu>
2025-04-28 18:19:55 -0700

    created grid, added info, added pictures, had to move public folder

--- src/app/components/MemberCard.tsx
+++ src/app/components/MemberCard.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Image from "next/image";
-import Pic from "@/Public/Rishitha Kona.png";
 interface MemberCardProps {
   position: string;
+  imageSrc: string;
   name: string;
   pronouns: string;
   info: string;
 }
 
-const MemberCard = ({ position, name, pronouns, info }: MemberCardProps) => {
+const MemberCard = ({
+  position,
+  imageSrc,
+  name,
+  pronouns,
+  info,
+}: MemberCardProps) => {
   return (
-    <div className="h-full w-1/2 rounded-3xl border-[2px] border-black bg-amber-300 p-5 text-center font-serif">
+    <div className="h-full w-3/5 rounded-3xl border-[3px] border-black p-5 text-center font-serif">
       <div className="text-4xl">{position}</div>
       <div className="">
         <Image
           className="mx-auto size-3/4"
-          src={Pic}
-          alt="Rishitha Kona"
+          src={imageSrc}
+          alt="picture of member"
+          width={200}
+          height={200}
         ></Image>
         <div className="text-2xl"> {name} </div>
         <div> ({pronouns}) </div>
