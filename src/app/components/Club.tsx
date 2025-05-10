d7e2429ab3c4b0e0e87e7da7e53026825a0a6c83
Nathan Matsuo <nathan@ucr-secure-21-10-13-135-113.wnet.ucr.edu>
2025-05-10 15:18:20 -0700

    added text and boxes as well as new webpage for opportunities

---
+++ src/app/components/Club.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Club = () => {
+  return (
+    <div className="flex h-screen w-screen flex-col px-40 py-5">
+      <h1 className="self-start p-8 text-3xl font-bold">Club</h1>
+      <p className="h-full w-full rounded-2xl border-2 border-[#B64D4C] p-8 text-center">
+        R'Brains and Behavior will arrange various social events with UCR staff
+        and alumni in order to allow members the opportunity to create networks
+        within and outside of the university. This organization will stand as a
+        support of each member as they find or expand their concentration within
+        the field of psychology.
+        <br />
+        <br />
+        <br />
+        learn more about R’BRAINS
+      </p>
+    </div>
+  );
+};
+
+export default Club;
