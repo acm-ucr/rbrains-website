df99e55999a3e0426cab82fe81b430c3cf40c731
Ricardo Galeano <ricardogaleano@Mac.home>
2025-05-20 15:09:26 -0700

    update so far for Stanley

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,15 @@
-import Gallery from "@/components/gallery/Gallery";
+import Gallery1 from "@/components/gallery/Gallery1";
+import Gallery2 from "@/components/gallery/Gallery2";
+import Gallery3 from "@/components/gallery/Gallery3";
+import Gallery4 from "@/components/gallery/Gallery4";
 
 const Page = () => {
   return (
     <div>
-      <Gallery />
+      <Gallery1 />
+      <Gallery2 />
+      <Gallery3 />
+      <Gallery4 />
     </div>
   );
 };
