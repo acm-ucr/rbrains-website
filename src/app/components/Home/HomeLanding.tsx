fc4f28a2e0255d67439547a767469f04c877c39b
Kelly Ramirez <157239272+kellyramirezz@users.noreply.github.com.>
2025-05-21 01:38:12 -0700

    updated homelanding page and changed .png to .svg

--- src/app/components/Home/HomeLanding.tsx
+++ src/app/components/Home/HomeLanding.tsx
@@ -3,51 +3,95 @@ import Image from "next/image";
 
 const HomeLanding = () => {
   return (
-    <>
-      <div className="flex items-center justify-between bg-[#F9F5EF]">
-        <div className="relative aspect-square flex-1">
+    <div className="bg-[#F9F5EF]">
+      <div className="flex w-full flex-row pb-10">
+        <div className="relative top-6 flex-1">
           <Image
-            className="object-contain"
-            src="/homelanding/squiggle1.png"
+            className="h-full w-full object-contain"
+            src="/homelanding/squiggle2.svg"
             alt="left squiggle"
             fill
           />
         </div>
 
         {/* main contents */}
-        <div className="relative flex aspect-square flex-1 items-center justify-center">
-          {/* content */}
-          <div className="relative h-64 w-84">
+        <div className="relative ml-6 flex flex-1 flex-col items-center justify-center">
+          <div className="z-10 w-full">
             <Image
-              className="object-contain"
-              src="/homelanding/rbrain_logo.png"
-              alt="home"
-              fill
+              className="h-auto w-full object-contain"
+              src="/homelanding/curvetext.svg"
+              alt="Welcome text"
+              width={0}
+              height={0}
+              sizes="100vw"
             />
-
-            <div className="absolute inset-0 flex items-center justify-center font-[Besley] font-semibold text-[#442F2C]">
-              <p className="text-3xl">To</p>
+          </div>
+          <div className="absolute top-[50%] z-10 flex w-full flex-none -translate-y-[30%] items-center justify-center">
+            <div className="w-1/8 max-w-[4rem]">
+              <Image
+                className="h-auto w-full object-contain"
+                src="/homelanding/TO.svg"
+                alt="TO text"
+                width={0}
+                height={0}
+                sizes="100vw"
+              />
+            </div>
+          </div>
+          <div className="relative z-10 h-auto w-full">
+            <div className="absolute right-0 bottom-0 left-0 -translate-y-[5%] transform">
+              <Image
+                className="h-auto w-full object-contain"
+                src="/homelanding/rbrainslogo.svg"
+                alt="rbrain logo"
+                width={0}
+                height={0}
+                sizes="100vw"
+              />
             </div>
           </div>
         </div>
         {/* end of main content */}
 
         {/* right imgage */}
-        <div className="relative aspect-square flex-1">
+        <div className="relative top-6 flex-1">
           <Image
-            className="object-contain"
-            src="/homelanding/squiggle2.png"
+            className="h-full w-full object-contain"
+            src="/homelanding/squiggle1.svg"
             alt="right squiggle"
             fill
           />
         </div>
       </div>
 
-      {/* bottom border */}
-      <div className="flex justify-center">
-        <div className="w-11/12 border-b-2 border-[#282220]" />
+      <div className="mx-auto flex w-11/12 items-center">
+        <div className="flex-grow border-b-2 border-[#282220]"></div>
+        <div className="relative mx-9 h-4 w-4">
+          <Image
+            className="absolute top-0 left-0"
+            src="/homelanding/heart3.svg"
+            alt="left circle"
+            width={9}
+            height={9}
+          />
+          <Image
+            className="absolute top-0 right-0"
+            src="/homelanding/heart2.svg"
+            alt="right circle"
+            width={9}
+            height={9}
+          />
+          <Image
+            className="absolute bottom-0 left-1/2 -translate-x-1/2 transform"
+            src="/homelanding/heart1.svg"
+            alt="bottom heart shape"
+            width={13}
+            height={13}
+          />
+        </div>
+        <div className="flex-grow border-b-2 border-[#282220]"></div>
       </div>
-    </>
+    </div>
   );
 };
 
